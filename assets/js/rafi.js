444422ff92d1c271be440efe5fc833dddfdcca16
rafiadadisini <lagirafi@gmail.com>
2024-06-05 09:39:08 +0700

    tambah POKEMON

--- assets/js/rafi.js
+++ assets/js/rafi.js
@@ -1,5 +1,28 @@
-let welcome = "Selamat datang di website portfolio @rafiadadisini";
-console.log(welcome);
+// Menambah Para pokemon
 
-let angkaRandom = Math.random() * 10;
-console.log("Angka Acak " + Math.round(angkaRandom));
+// url
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png
+
+const rumahpokemon = document.querySelector(".para-pokemon");
+const linkpokemon =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/";
+
+for (i = 1; i <= 25; i++) {
+  let pokeball = document.createElement("div");
+  pokeball.classList.add("col-3");
+  pokeball.classList.add("text-center");
+
+  const gambarpoke = document.createElement("img");
+  gambarpoke.classList.add("img-thumbnail");
+  gambarpoke.classList.add("m-1");
+  gambarpoke.classList.add("rounded-circle");
+
+  gambarpoke.src = `${linkpokemon}${i}.png`;
+
+  let textpoke = document.createElement("h5");
+  textpoke.append(`#${i}`);
+
+  rumahpokemon.appendChild(pokeball);
+  pokeball.appendChild(gambarpoke);
+  pokeball.appendChild(textpoke);
+}
